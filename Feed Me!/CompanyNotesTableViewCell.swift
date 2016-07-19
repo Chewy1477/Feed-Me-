55217b68c3d183927fe1884932bbea6184abc43f
Chris Chueh <chc3cc@Virginia.edu>
2016-07-19 13:46:56 -0400

    Working on table view

---
+++ Feed Me!/CompanyNotesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CompanyNotesTableViewCell.swift
+//  Feed Me!
+//
+//  Created by Administrator on 7/19/16.
+//  Copyright © 2016 Chris Chueh. All rights reserved.
+//
+
+import UIKit
+
+class CompanyNotesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
